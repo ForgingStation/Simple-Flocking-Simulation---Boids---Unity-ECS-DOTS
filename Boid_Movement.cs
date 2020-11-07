e09393357135820cd20d7d1079b3f63652a3a6b1
Forging Station <67524557+ForgingStation@users.noreply.github.com>
2020-11-07 12:59:19 +0000

    Add files via upload

---
+++ Boid_Movement.cs
@@ -0,0 +1,93 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+using Unity.Collections;
+
+public class Boid_Movement : SystemBase
+{
+    public NativeMultiHashMap<int, Boid_ComponentData> cellVsEntityPositions;
+
+    public static int GetUniqueKeyForPosition(float3 position, int cellSize)
+    {
+        return (int)((15 * math.floor(position.x / cellSize)) + (17 * math.floor(position.y / cellSize)) + (19 * math.floor(position.z / cellSize)));
+    }
+
+    protected override void OnCreate()
+    {
+        cellVsEntityPositions = new NativeMultiHashMap<int, Boid_ComponentData>(0, Allocator.Persistent);
+    }
+
+    protected override void OnUpdate()
+    {
+        EntityQuery eq = GetEntityQuery(typeof(Boid_ComponentData));
+        cellVsEntityPositions.Clear();
+        if (eq.CalculateEntityCount() > cellVsEntityPositions.Capacity)
+        {
+            cellVsEntityPositions.Capacity = eq.CalculateEntityCount();
+        }
+
+        NativeMultiHashMap<int, Boid_ComponentData>.ParallelWriter cellVsEntityPositionsParallel = cellVsEntityPositions.AsParallelWriter();
+        Entities.ForEach((ref Boid_ComponentData bc, ref Translation trans) =>
+        {
+            Boid_ComponentData bcValues = new Boid_ComponentData();
+            bcValues = bc;
+            bcValues.currentPosition = trans.Value;
+            cellVsEntityPositionsParallel.Add(GetUniqueKeyForPosition(trans.Value, bc.cellSize), bcValues);
+        }).ScheduleParallel();
+
+        float deltaTime = Time.DeltaTime;
+        NativeMultiHashMap<int, Boid_ComponentData> cellVsEntityPositionsForJob = cellVsEntityPositions;
+        Entities.WithBurst().WithReadOnly(cellVsEntityPositionsForJob).ForEach((ref Boid_ComponentData bc, ref Translation trans, ref Rotation rot) =>
+        {
+            int key = GetUniqueKeyForPosition(trans.Value, bc.cellSize);
+            NativeMultiHashMapIterator<int> nmhKeyIterator;
+            Boid_ComponentData neighbour;
+            int total = 0;
+            float3 separation = float3.zero;
+            float3 alignment = float3.zero;
+            float3 coheshion = float3.zero;
+
+            if (cellVsEntityPositionsForJob.TryGetFirstValue(key, out neighbour, out nmhKeyIterator))
+            {
+                do
+                {
+                    if (!trans.Value.Equals(neighbour.currentPosition) && math.distance(trans.Value, neighbour.currentPosition) < bc.perceptionRadius)
+                    {
+                        float3 distanceFromTo = trans.Value - neighbour.currentPosition;
+                        separation += (distanceFromTo / math.distance(trans.Value, neighbour.currentPosition));
+                        coheshion += neighbour.currentPosition;
+                        alignment += neighbour.velocity;
+                        total++;
+                    }
+                } while (cellVsEntityPositionsForJob.TryGetNextValue(out neighbour, ref nmhKeyIterator));
+                if (total > 0)
+                {
+                    coheshion = coheshion / total;
+                    coheshion = coheshion - (trans.Value+bc.velocity);
+                    coheshion = math.normalize(coheshion) * bc.cohesionBias;
+
+                    separation = separation / total;
+                    separation = separation - bc.velocity;
+                    separation = math.normalize(separation) * bc.separationBias;
+
+                    alignment = separation / total;
+                    alignment = alignment - bc.velocity;
+                    alignment = math.normalize(alignment) * bc.alignmentBias;
+
+                }
+
+                bc.acceleration += (coheshion + alignment + separation);
+                rot.Value = math.slerp(rot.Value, quaternion.LookRotation(math.normalize(bc.velocity), math.up()), deltaTime * 10);
+                bc.velocity = bc.velocity + bc.acceleration;
+                bc.velocity = math.normalize(bc.velocity) * bc.speed;
+                trans.Value = math.lerp(trans.Value, (trans.Value+bc.velocity), deltaTime * bc.step);
+                bc.acceleration = math.normalize(bc.target - trans.Value) * bc.targetBias;
+            }
+        }).ScheduleParallel();
+    }
+
+    protected override void OnDestroy()
+    {
+        cellVsEntityPositions.Dispose();
+    }
+}
---
+++ Boid_Spawner.cs
@@ -0,0 +1,86 @@
+﻿using Unity.Entities;
+using UnityEngine;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Rendering;
+
+public class Boid_Spawner : MonoBehaviour
+{
+    public int boidsPerInterval;
+    public int boidsToSpawn;
+    public float interval;
+    public float cohesionBias;
+    public float separationBias;
+    public float alignmentBias;
+    public float targetBias;
+    public float perceptionRadius;
+    public float3 target;
+    public Material material;
+    public Mesh mesh;
+    public float maxSpeed;
+    public float step;
+    public int cellSize;
+    private EntityManager entitymanager;
+    private Entity entity;
+    private float elapsedTime;
+    private int totalSpawnedBoids;
+    private EntityArchetype ea;
+    private float3 currentPosition;
+
+    private void Start()
+    {
+        totalSpawnedBoids = 0;
+        entitymanager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        currentPosition = this.transform.position;
+        ea = entitymanager.CreateArchetype(
+            typeof(Translation),
+            typeof(Rotation),
+            typeof(LocalToWorld),
+            typeof(RenderMesh),
+            typeof(RenderBounds),
+            typeof(Boid_ComponentData)
+            );
+    }
+
+    private void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime >= interval)
+        {
+            elapsedTime = 0;
+            for (int i=0; i<=boidsPerInterval; i++)
+            {
+                if (totalSpawnedBoids == boidsToSpawn)
+                {
+                    break;
+                }
+                Entity e = entitymanager.CreateEntity(ea);
+
+                entitymanager.AddComponentData(e, new Translation
+                {
+                    Value = currentPosition
+                });
+                entitymanager.AddComponentData(e, new Boid_ComponentData
+                {
+                    velocity = math.normalize(UnityEngine.Random.insideUnitSphere) * maxSpeed,
+                    perceptionRadius = perceptionRadius,
+                    speed = maxSpeed,
+                    step = step,
+                    cohesionBias = cohesionBias,
+                    separationBias = separationBias,
+                    alignmentBias = alignmentBias,
+                    target = target,
+                    targetBias = targetBias,
+                    cellSize = cellSize
+                });
+                entitymanager.AddSharedComponentData(e, new RenderMesh
+                {
+                    mesh = mesh,
+                    material = material,
+                    castShadows = UnityEngine.Rendering.ShadowCastingMode.On
+                });
+                totalSpawnedBoids++;
+            }
+        }
+    }
+}
